93917f7c4c949518cb738156b42586e435c4f089
rhdqngkwk90 <ouorhdqngkwk90@gmail.com>
2024-10-16 16:30:09 +0900

    초기 프로젝트 세팅

---
+++ ii-api/src/main/java/com/ureca/controller/TestController.java
@@ -0,0 +1,26 @@
+package com.ureca.controller;
+
+import com.ureca.service.TestService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/test")
+@Controller
+public class TestController {
+    private static final Logger logger = LoggerFactory.getLogger(TestController.class);
+
+    private TestService testService;
+
+    public TestController(TestService testService) {
+        this.testService = testService;
+    }
+
+    @GetMapping("/view")
+    public void view() {
+        testService.setTicketInfo("야이거다");
+        logger.info("호출 성공!!!!");
+    }
+}
---
+++ ii-domain/src/main/java/com/ureca/service/TestService.java
@@ -0,0 +1,14 @@
+package com.ureca.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+    private static final Logger logger = LoggerFactory.getLogger(TestService.class);
+
+    public void setTicketInfo(String id) {
+        logger.info("id { " + id + " }");
+    }
+}
